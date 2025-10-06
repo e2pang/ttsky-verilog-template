0e7e0bd7e3bc5572f2c0e9ca5abde1ddb744000e
Ethan Pang <e2pang@uwaterloo.ca>
2025-10-06 13:10:36 -0400

    initial commit

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,27 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  assign uio_oe = 8'b0;         // keep IO's on input always
+
+  reg [7:0] temp;             
+  
+  wire load_en = ui_in[0];       // turn on of the inputs into a load-enable signal
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      temp <= 8'b0;             // asynchronous reset
+    else if (load_en)
+      temp <= uio_in;           // synchronous load (checks load signal on each clock cycle)
+    else
+      temp <= temp + 1;         // if no load and no reset, then count upwards by 1
+  end
+
+  assign uo_out = ena ? temp : 8'bz; // uses the value of ena to decide whether to output counter value or go high-z
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{clk, 1'b0};
 
 endmodule
